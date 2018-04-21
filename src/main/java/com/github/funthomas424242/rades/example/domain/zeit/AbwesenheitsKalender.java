f7f9fa35288da31bf43d848d51a38ee574deffb1
FunThomas424242 <funthomas424242@gmail.com>
2018-04-21 19:59:00 +0200

    initiales Test setup
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/main/java/com/github/funthomas424242/rades/example/domain/zeit/AbwesenheitsKalender.java
+++ src/main/java/com/github/funthomas424242/rades/example/domain/zeit/AbwesenheitsKalender.java
@@ -1,14 +1,31 @@
 package com.github.funthomas424242.rades.example.domain.zeit;
 
+import com.github.funthomas424242.rades.example.domain.DomainObject;
 import com.github.funthomas424242.rades.example.domain.firma.Team;
+import com.github.funthomas424242.rades.example.domain.firma.TeamAccessor;
+import com.github.funthomas424242.rades.example.domain.personal.PersonAccessor;
 
 import javax.validation.constraints.NotNull;
 import java.util.Set;
 
+@DomainObject
 public class AbwesenheitsKalender {
 
     @NotNull
     protected Set<Team> teams;
 
 
+    public void printAbwesenheiten() {
+        final StringBuffer buf = new StringBuffer();
+        teams.forEach(team -> {
+            final TeamAccessor curTeam = new TeamAccessor(team);
+            buf.append(curTeam.getTeamName() + "\n");
+            curTeam.getMitglieder().forEach(person -> {
+                final PersonAccessor curPerson = new PersonAccessor(person);
+                buf.append(curPerson.getNachname() + ", " + curPerson.getVorname());
+                // TODO Abwesenheiten
+            });
+        });
+        System.out.println(buf.toString());
+    }
 }
--- src/test/java/com/github/funthomas424242/rades/example/domain/zeit/AbwesenheitsKalenderTest.java
+++ src/test/java/com/github/funthomas424242/rades/example/domain/zeit/AbwesenheitsKalenderTest.java
@@ -1,11 +1,18 @@
 package com.github.funthomas424242.rades.example.domain.zeit;
 
 import com.github.funthomas424242.rades.example.domain.NameBuilder;
+import com.github.funthomas424242.rades.example.domain.firma.Team;
+import com.github.funthomas424242.rades.example.domain.firma.TeamAccessor;
+import com.github.funthomas424242.rades.example.domain.firma.TeamBuilder;
 import com.github.funthomas424242.rades.example.domain.personal.Person;
+import com.github.funthomas424242.rades.example.domain.personal.PersonAccessor;
 import com.github.funthomas424242.rades.example.domain.personal.PersonBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -30,6 +37,21 @@ class AbwesenheitsKalenderTest {
                 .withNachname(new NameBuilder().withWert("Musterfrau").build())
                 .build();
         assertNotNull(kathrin);
+        final Abwesenheit jahresurlaubKathrin = new AbwesenheitBuilder()
+                .withGrund(Abwesenheit.Grund.URLAUB)
+                .withPeriode(LocalDate.of(2018,7,1).until(LocalDate.of(2018,7,31)))
+                .build();
+        assertNotNull(jahresurlaubKathrin);
+        final PersonAccessor kathrinAccessor=new PersonAccessor(kathrin);
+        kathrinAccessor.getAbwesenheiten().add(jahresurlaubKathrin);
+
+        final Team team4 = new TeamBuilder().withTeamName(new NameBuilder().withWert("Team 4").build()).withMitglieder(new HashSet<Person>()).build();
+        final TeamAccessor team4Accessor = new TeamAccessor(team4);
+        team4Accessor.getMitglieder().add(kathrin);
+
+        final AbwesenheitsKalenderAccessor abwesenheitsKalenderAccessor = new AbwesenheitsKalenderAccessor(kalender);
+        abwesenheitsKalenderAccessor.getTeams().add(team4);
+        kalender.printAbwesenheiten();
 
     }
 
